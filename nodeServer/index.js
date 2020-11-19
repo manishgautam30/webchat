3d3eead0426a6db15ebb74123fce71a9efcf54e8
manishgautam30 <manish.gautam_cs18@gla.ac.in>
2020-11-20 01:11:57 +0530

    done

--- nodeServer/index.js
+++ nodeServer/index.js
@@ -17,6 +17,7 @@ io.on('connection', socket => {
 
     socket.on('disconnect', message => {
         socket.broadcast.emit('left', users[socket.id])
+        delete users[socket.id];
     });
 
 });
